6546b8084182cd6ae9fbdcaf749000ac106d58c0
Eugene Clark <clarkjr2016@gmail.com>
2023-11-06 19:59:44 -0500

    Added hit and isSunk methods

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -1,12 +1,30 @@
 import { Ship } from "../functionality/ship";
 
 it("Seeing if hit method adds 1", () => {
-  const test = new Ship(5, 0, false);
+  const test = new Ship(5);
 
   expect(test.hit()).toBe(1);
 });
 
 it("Seeing if the getLength function returns the initialized length", () => {
-  const test = new Ship(5, 0, false);
+  const test = new Ship(5);
+
   expect(test.getLength()).toBe(5);
+});
+
+it("Seeing if the sunk function will return false if length is higher than hits", () => {
+  const test = new Ship(2);
+
+  test.hit(); //adding one to the hit property
+
+  expect(test.isSunk()).toBe(false);
+});
+
+it("Seeing if the sunk function will return true if length is equal to the hits", () => {
+  const test = new Ship(2);
+
+  test.hit(); //adding one to the hit property
+  test.hit(); //adding one more to the property to equal 2
+
+  expect(test.isSunk()).toBe(true);
 });
--- src/functionality/ship.js
+++ src/functionality/ship.js
@@ -1,8 +1,8 @@
 export class Ship {
-  constructor(length, hits, sunk) {
+  constructor(length) {
     this.length = length;
-    this.hits = hits;
-    this.sunk = sunk;
+    this.hits = 0; // hits will always start off with 0
+    this.sunk = false; // ships will always start off with a false sunk status
   }
 
   getLength() {
@@ -10,6 +10,18 @@ export class Ship {
   }
 
   hit() {
-    return this.hits + 1; // increases this objects hit count by 1;
+    return (this.hits += 1); // increases this objects hit count by 1;
+  }
+
+  getHits() {
+    return this.hits;
+  }
+
+  isSunk() {
+    if (this.getLength() <= this.getHits()) {
+      return (this.sunk = true);
+    } else {
+      return this.sunk;
+    }
   }
 }
